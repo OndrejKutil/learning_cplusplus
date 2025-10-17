f9b2387ed11dd470523161fb092e23ac24b3b5f6
Ondřej Kutil <kutil.ondrej@outlook.com>
2025-10-17 10:48:27 +0200

    comments

--- src/first_steps/portfolio.cpp
+++ src/first_steps/portfolio.cpp
@@ -30,6 +30,7 @@ struct Order {
 class Portfolio {
     public:
         void addOrder(const Order &order) {
+            // add order to the back of orders vector
             orders.push_back(order);
 
             if (order.type == OrderType::BUY) {
@@ -48,6 +49,9 @@ class Portfolio {
             }
             else {
                 std::cout << "Current Portfolio Positions:" << std::endl;
+                // & for direct access without copying
+                // we want exactly what is in positions vector
+                // const so we don't modify it
                 for (const Position &pos : positions) {
                     if (pos.quantity > 0) {
                         std::cout << "Ticker: " << pos.ticker 
@@ -98,6 +102,8 @@ class Portfolio {
         }
 
     private:
+        // Internal storage for positions and orders
+        // Vectors can dynamically resize
         std::vector<Position> positions;
         std::vector<Order> orders;
 
@@ -121,6 +127,7 @@ class Portfolio {
                         return;
                     }
                     existingPos.quantity -= quantity; // Update quantity
+                    // no problem if quantity becomes zero, we will clean it up later
 
                     removeZeroQuantityPositions(); // Clean up zero quantity positions
                     return;
@@ -130,7 +137,17 @@ class Portfolio {
         }
 
         void removeZeroQuantityPositions() {
-            positions.erase(std::remove_if(positions.begin(), positions.end(),
+            // erase(start iterator, end iterator), erase is left inclusive, right exclusive [left, right)
+            // vect.begin() returns iterator to the first element
+            // vect.end() returns iterator to one past the last element
+            // remove_if(start iterator, end iterator, condition) -> iterator to the logical end of the range
+            // remove_if shifts all elements that do not meet the condition to the front of the range, keeps the ones that meet the condition at the end
+            // keeps relative order of elements that do not meet the condition
+            // returns iterator to the new logical end of the range (first element to be removed)
+            // lambda function: [capture](parameters) -> return_type { body }
+            // We capture nothing, take a Position reference, return bool for every position that meets the condition
+            // The erase() then sees only a range starting from the first element to be removed to the actual end of the vector (one after) and removes that range
+            positions.erase(std::remove_if(positions.begin(), positions.end(), 
                 [](const Position &pos) { return pos.quantity == 0; }),
                 positions.end());
         }
